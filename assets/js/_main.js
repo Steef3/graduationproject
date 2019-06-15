9ba89c7ab7102285891eb3dbfd5d2375e9e3c251
Steef3 <stefan.hiebl@gmx.com>
2019-06-14 21:59:10 -0500

    Update _main.js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
     $(document).on('click', '.warning-dismiss', function (e) {
       e.preventDefault();
       $.magnificPopup.close();
-    }, 100000);
+    });
   });
   
   // Sticky footer
